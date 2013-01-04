33e5ddf90cc39edbb4f056c3584b296265da516b
Joel <epci_joelms@hotmail.com>
2013-01-04 18:23:04 -0500

    Mi primer archivo subido a GitHub
    Es una prueba porque estoy empezando con GitHub

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html  dir="ltr" lang="es" xml:lang="es" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+        <link rel="openid.server" href="http://joel7.myopenid.com/"/>
+        <link rel="openid.delegate" href="http://joel7.com/"/>
+    <title>Aula Virtual</title>
+    <link rel="shortcut icon" href="http://joel7.com/aulavirtual/theme/image.php?theme=darkb&amp;image=favicon&amp;rev=199&amp;component=theme" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="moodle, Aula Virtual" />
+</head>
+<body id="page-site-index" class="course path-site ie ie8 dir-ltr lang-es yui-skin-sam yui3-skin-sam joel7-com--aulavirtual pagelayout-frontpage course-1 context-2 notloggedin has_custom_menu">
+    <table width="982" height="116" border="0" cellpadding="0" cellspacing="0" align="center" >
+      <tr>
+        <td width="766" height="94"  valign="top"><center>
+
+              <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,19,0" width="772" height="94" >
+                <param name="wmode" value="transparent">
+			    <param name="movie" value="/flash/bannerJoel.swf">
+                <param name="quality" value="high">
+                <embed src="/flash/bannerJoel.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="772" height="94"></embed>
+              </object>
+
+          </center>
+        </td>
+<!--        <td rowspan="2">
+        <iframe title="" height="167"	 width="175" src="http://joel7.com/bethelTv.php" frameborder="0" marginheight="0" marginwidth="0"  allowfullscreen></iframe>
+
+        </td>-->
+      </tr>
+      <tr>
+        <td height="20"  valign="top">
+ 
+           <table>
+             <tr>
+              <td>
+                 <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,19,0" width="688" height="20" title="" >
+
+        			<param name="wmode" value="transparent">
+                    <param name="movie" value="/flash/textBienvenido.swf">
+                    <param name="quality" value="high">
+                    <embed src="/flash/textBienvenido.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="688" height="20"> </embed>
+                  </object>
+              </td>
+              <td>
+                  <a href="http://joel7.com/colabora.php" target="_blank"> <img alt="" border="0" src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif" ></a>
+              </td>
+             </tr>
+            </table>
+
+        </td>
+      </tr>
+      <tr>
+              <td>
+                    <ul>
+			  <li>Clic <a href="http://joel7.com/aula" target="_blank" title="Clic aquí">aquí</a>  para poder acceder al <b>Aula Virtual</b>. Gracias.</li><br>
+			  <li>Clic <a href="http://tiendaj.com/" target="_blank" title="Clic aquí">aquí</a>  para poder acceder a la <b>Tienda Virtual</b>. Gracias.</li><br>
+			  <li>Clic <a href="http://jovenmisionero.com/" target="_blank" title="Clic aquí">aquí</a>  para poder acceder a la <b>Página Web de la Sociedad de Jóvenes del MMM</b>. Gracias.</li>
+		    </ul>
+              </td>
+      </tr>
+    </table>
+	  
+</body>
+</html>
